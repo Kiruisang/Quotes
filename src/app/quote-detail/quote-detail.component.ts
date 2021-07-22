7c964e2607fd0b698a38fcdb52a5267b62ebac6d
Kirui sang <brian.kirui@student.moringaschool.com>
2021-07-22 10:36:19 +0300

    final commit of the project

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+   
+  @Input() quote!:Quote;
+  @Output() isCreate = new EventEmitter<boolean>();
+  
+  quoteDelete(create:boolean){
+    this.isCreate.emit(create);
+  }
+  @Output() isUpVote = new EventEmitter<boolean>();
+  @Output() isDownVote = new EventEmitter<boolean>();
 
+  upVote(like:boolean){
+    this.isUpVote.emit(like);
+  }
+  downVote(dislike:boolean){
+    this.isDownVote.emit(dislike);
+  }
   constructor() { }
 
   ngOnInit(): void {
